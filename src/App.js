e01c8cef3cb722ae6d78af91eb805db6dfd2a75c
JexXxika <wizcajesse@gmail.com>
2022-11-02 07:07:13 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import Github from './img/IconGithub.png'
+import Logo from './img/logo.png'
+import Slack from './img/slackslack.png'
+import {FiCamera} from 'react-icons/fi'
+import { RiShareForwardLine } from 'react-icons/ri'
+import Profile from './img/Profile.jpg'
+ 
+
 
 function App() {
+
+  
+  const links = [{
+    id: "twitter",
+    href: 'https://twitter.com/_JexXxika',
+    title : 'Twitter Link'
+  },
+    {
+     id:'btn_zuri',
+      href: 'http://books.zuri.team',
+      title:'Zuri Team'
+    },
+    {
+    id:'book',
+      href: 'https://training.zuri.team/',
+     title:'Zuri Books'
+    },
+  {href:'https://books.zuri.team/python-for-beginners?ref_id=<O>',
+      id: 'book_phyton',
+      title: 'Phyton Books'
+    },
+    {
+      href:'https://background.zuri.team',
+            id:"pitch",
+    title:'Background Check for coders'
+    },
+    {
+      href:'https://books.zuri.team/design-rules',
+             id:"book_design",
+    title:'Design Books'
+  },
+  ]
+  // using setState for the modal
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="shareimg">
+          <div className='img'>
+            <FiCamera className='camera'/>
+            <img src={Profile} alt="Jessica" ></img>
+          </div>
+          
+          <RiShareForwardLine title='Share link' className='share'/>
+        </div>
+        <h1 id='twitter'>JexXxika</h1>
       </header>
-    </div>
+      
+      <section>
+        <ul>
+          {links.map(link => <li><a 
+            key={link.id}
+            id={link.id}
+            href={link.href}
+            target='_blank'
+            rel="noreferrer">{ link.title}</a></li>)}
+        </ul>
+
+        <div className='icons'>
+        <img src={ Github} alt='github icon' />
+        <img src={Slack } alt='slack icon' />
+        </div>
+      </section>
+      <footer>
+        <h1>Zuri <span>.</span> Internship</h1>
+        <h3>HNG Internship 9 Frontend Task</h3>
+        <img src={Logo } alt='ingressive for good logo' />
+      </footer>
+    </>
   );
 }
 
